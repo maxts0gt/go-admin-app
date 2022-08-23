f0d1f520ccf76af22aba676b83ecc96d43123d4f
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-08-23 18:20:40 +0900

    setup database

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":8000")
 }
