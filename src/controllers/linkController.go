1cbafbc1cce22d342119d32c5cd598084cd688af
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-08-31 22:26:28 +0900

    feat: setup links

---
+++ src/controllers/linkController.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/maxts0gt/go-ambassador/src/database"
+	"github.com/maxts0gt/go-ambassador/src/models"
+)
+
+func Link(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	var links []models.Link
+
+	database.DB.Where("user_id = ?", id).Find(&links)
+
+	return c.JSON(links)
+}
