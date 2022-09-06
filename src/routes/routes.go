74d7ed3f84125a0ed4f9ea891b733ee90b8d3b36
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-09-06 23:30:10 +0900

    feat: create rankings

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -42,4 +42,5 @@ func Setup(app *fiber.App) {
 	ambassadorAuthenticated.Put("users/password", controllers.UpdatePassword)
 	ambassadorAuthenticated.Post("links", controllers.CreateLink)
 	ambassadorAuthenticated.Get("stats", controllers.Stats)
+	ambassadorAuthenticated.Get("rankings", controllers.Rankings)
 }
