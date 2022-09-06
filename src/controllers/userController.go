fa5ad13d10600d708cf09ccf7fd7af81a4ead8a9
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-09-06 23:44:05 +0900

    feat: sort results in redis

--- src/controllers/userController.go
+++ src/controllers/userController.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 	"github.com/maxts0gt/go-ambassador/src/database"
 	"github.com/maxts0gt/go-ambassador/src/models"
@@ -13,4 +16,26 @@ func Ambassadors(c *fiber.Ctx) error {
 	database.DB.Where("is_ambassador = true").Find(&users)
 
 	return c.JSON(users)
+}
+
+func Rankings(c *fiber.Ctx) error {
+
+	// redis returning result
+	rankings, err := database.Cache.ZRevRangeByScoreWithScores(context.Background(), "rankings", &redis.ZRangeBy{
+		Min: "-inf",
+		Max: "+inf",
+	}).Result()
+
+	if err != nil {
+		return err
+	}
+	// creating map
+	result := make(map[string]float64)
+
+	// matching result and score as value
+	for _, ranking := range rankings {
+		result[ranking.Member.(string)] = ranking.Score
+	}
+
+	return c.JSON(result)
 }
