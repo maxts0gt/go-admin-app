ddcd0804edf34b88e9bfc28ccce0d160768149af
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-09-06 23:16:58 +0900

    feat: create links

--- src/controllers/linkController.go
+++ src/controllers/linkController.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"strconv"
 
+	"github.com/bxcodec/faker/v3"
 	"github.com/gofiber/fiber/v2"
 	"github.com/maxts0gt/go-ambassador/src/database"
+	"github.com/maxts0gt/go-ambassador/src/middlewares"
 	"github.com/maxts0gt/go-ambassador/src/models"
 )
 
@@ -24,4 +26,34 @@ func Link(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(links)
+}
+
+type CreateLinkRequest struct {
+	Products []int
+}
+
+func CreateLink(c *fiber.Ctx) error {
+	var request CreateLinkRequest
+
+	if err := c.BodyParser(&request); err != nil {
+		return err
+	}
+
+	id, _ := middlewares.GetUserId(c)
+
+	link := models.Link{
+		UserId: id,
+		Code:   faker.Username(),
+	}
+
+	for _, productId := range request.Products {
+		product := models.Product{}
+		product.Id = uint(productId)
+		link.Products = append(link.Products, product)
+	}
+
+	database.DB.Create(&link)
+
+	return c.JSON(link)
+
 }
